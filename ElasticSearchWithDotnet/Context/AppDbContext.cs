efb51717a46116dbc6eef500d3565cfff2255ddc
Barış Gün <108459412+bgun7@users.noreply.github.com>
2024-12-20 18:27:04 +0300

    add dbcontext and first migration

---
+++ ElasticSearchWithDotnet/Context/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ElasticSearchWithDotnet.Context
+{
+    public sealed class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=LAPTOP-TKFJC4RO\\SQLEXPRESS;Initial Catalog= TestDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+
+        public DbSet<Travel> Travels { get; set; }
+    }
+}
+
+public sealed class Travel
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+}
